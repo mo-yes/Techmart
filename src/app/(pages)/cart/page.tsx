9dc5db579ff70c08cf39c0b902c536984a63faf9
mo-yes <mohamed490216@gmail.com>
2025-09-29 05:51:22 +0300

    fix: force dynamic rendering for cart page with no cache

--- src/app/(pages)/cart/page.tsx
+++ src/app/(pages)/cart/page.tsx
@@ -1,16 +1,12 @@
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 import React from "react";
 import InnerCart from "./InnerCart";
 import { getUserCart } from "@/services/cart.services";
 
 export default async function Cart() {
-  async function fetchCartProducts() {
-    const response = await getUserCart();
-    return response;
-  }
-
-  const response = await fetchCartProducts();
+  const response = await getUserCart();
 
   return (
     <section className="container mx-auto px-4 py-8">

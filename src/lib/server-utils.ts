50b8ff77a512cf591670e5fc656925a7cede7f4c
mo-yes <mohamed490216@gmail.com>
2025-10-01 17:16:20 +0300

    fix: use NEXTAUTH_SECRET in server-utils

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -6,7 +6,7 @@ import { cookies } from "next/headers";
 export async function getUserToken(){
     const cockieStore =await cookies()
     const encodedToken = cockieStore.get('next-auth.session-token')?.value || 
-    cockieStore.get('__Secure-next-auth.callback-url')?.value
-    const decodedToken = await decode({token: encodedToken , secret:process.env.AUTH_SECRET!})
+    cockieStore.get('__Secure-next-auth.session-token')?.value
+    const decodedToken = await decode({token: encodedToken , secret:process.env.NEXTAUTH_SECRET!})
     return decodedToken?.token;
 }
